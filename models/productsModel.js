e28bcd96c0f3588c34336ba238251feb450d4830
KerliS9 <kerlischroeder9@gmail.com>
2022-05-25 17:53:31 -0300

    feat - incluindo mais testes para o model

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,7 +1,8 @@
 const connection = require('../db/connection');
 
 const getAllProducts = () => {
-
+  const query = 'SELECT * FROM StoreManager.products;';
+  return connection.execute(query);
 };
 
 module.exports = {
