35df44b7266a2f32f249af3c886998071c413278
KerliS9 <kerlischroeder9@gmail.com>
2022-05-25 18:39:03 -0300

    feat - adicionando productsControllers a rota

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,7 +1,8 @@
 const ProductsServices = require('../services/productsServices');
 
-const getAllProducts = () => {
-
+const getAllProducts = (_req, res) => {
+  const products = ProductsServices.getAllProducts();
+  return res.status(200).json(products);
 };
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
+const ProductsControllers = require('../controllers/productsControllers');
+
+/* routes.get('/', (req, res) => {
   return res.status(200).json('rota de products');
-});
+}); */
+
+routes.get('/', ProductsControllers.getAllProducts);
 
 module.exports = routes;
