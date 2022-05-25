c05bf2c78523d04aaae7ceb37dd186bde6128a1f
KerliS9 <kerlischroeder9@gmail.com>
2022-05-25 16:48:44 -0300

    feat - error handler

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = require('./app');
+const { errorHandler } = require('./middlewares/errorHandler');
 const routes = require('./routes');
 require('dotenv').config();
 
 app.use(express.json());
 
 app.use('/products', routes);
+
+app.use(errorHandler);
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 app.listen(process.env.PORT, () => {
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, _req, res, _next) => {
+  if (err.statusCode) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res.status(500).json({ message: 'Internal error' });
+};
+
+module.exports = {
+  errorHandler,
+};
