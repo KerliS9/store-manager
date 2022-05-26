74724c86865b3b8ec9a5c85b6b2dd75fd872c466
KerliS9 <kerlischroeder9@gmail.com>
2022-05-26 17:02:23 -0300

    feat - camada controller by  id

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,12 +2,15 @@ const ProductsServices = require('../services/productsServices');
 
 const getAllProducts = async (_req, res) => {
   const products = await ProductsServices.getAllProducts();
-  // console.log('controller', products);
   return res.status(200).json(products);
 };
 
 const getProductById = async (req, res) => {
-  
+  const { id } = req.params;
+  const [product] = await ProductsServices.getProductById(id);
+  console.log('controller', product);
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+  return res.status(200).json(product);
 };
 
 module.exports = {
