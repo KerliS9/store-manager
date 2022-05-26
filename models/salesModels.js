d0857c04ddd4471792ae9a6295ba98323430e027
KerliS9 <kerlischroeder9@gmail.com>
2022-05-26 18:41:40 -0300

    feat - camada model get sale by id

--- models/salesModels.js
+++ models/salesModels.js
@@ -6,15 +6,15 @@ const getAllSales = async () => {
   return sales;
 };
 
-/* const getProductById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.products WHERE id = ?;';
-  const [product] = await connection.execute(query, [id]);
-  // console.log('camada model', product[0]);
-  if (product.length === 0) return false;
-  return product[0];
-}; */
+const getSaleById = async (id) => {
+  const query = 'SELECT * FROM StoreManager.sales WHERE id = ?;';
+  const [sale] = await connection.execute(query, [id]);
+  // console.log('camada model', sale[0]);
+  if (sale.length === 0) return false;
+  return sale[0];
+};
 
 module.exports = {
   getAllSales,
-  // getProductById,
+  getSaleById,
 };
