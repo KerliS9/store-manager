9d0ad8391b790b931d54ee4918ff7329652bdbbe
KerliS9 <kerlischroeder9@gmail.com>
2022-05-27 19:37:14 -0300

    feat - camada service adicionar venda

--- services/salesServices.js
+++ services/salesServices.js
@@ -7,12 +7,26 @@ const getAllSales = async () => {
 
 const getSaleById = async (id) => {
   const sale = await SalesModels.getSaleById(id);
-  // console.log('camada service', sale);
   if (!sale) return null;
   return sale;
+};
+
+const addNewSale = async ({ productId, quantity }) => {
+  const newSale = await SalesModels.addNewProduct({ productId, quantity });
+  console.log('camada service', newSale);
+
+  return { statusCode: 201, newSale };
 };
 
 module.exports = {
   getAllSales,
   getSaleById,
+  addNewSale,
 };
+
+/* const addNewProduct = async ({ name, quantity }) => {
+  const productExistsOnDB = await ProductsModels.getProductByName(name);
+  if (productExistsOnDB) return ({ statusCode: 409, message: 'Product already exists' });
+  const newProduct = await ProductsModels.addNewProduct({ name, quantity });
+  return { statusCode: 201, newProduct };
+}; */
