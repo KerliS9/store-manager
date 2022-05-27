cf59993b1f6f707c4dd7514248cc7ad83898eabd
KerliS9 <kerlischroeder9@gmail.com>
2022-05-27 19:41:36 -0300

    feat - camada controller - adicionando nova venda

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -8,12 +8,35 @@ const getAllSales = async (_req, res) => {
 const getSaleById = async (req, res) => {
   const { id } = req.params;
   const sale = await SalesServices.getSaleById(id);
-  // console.log('controller', sale);
   if (sale === null) return res.status(404).json({ message: 'Sale not found' });
   return res.status(200).json(sale);
+};
+
+const addNewSale = async (req, res, next) => {
+  const { productId, quantity } = req.body;
+  console.log('controller req.body: ', req.body);
+  const { statusCode, message, newSale } = await SalesServices.addNewSale({
+    productId, quantity,
+  });
+  if (message) {
+    return next({ statusCode, message });
+  }
+  return res.status(statusCode).json(newSale);
 };
 
 module.exports = {
   getAllSales,
   getSaleById,
+  addNewSale,
 };
+
+/* const addNewProduct = async (req, res, next) => {
+  const { name, quantity } = req.body;
+  const { statusCode, message, newProduct } = await ProductsServices.addNewProduct({ 
+    name, quantity,
+  });
+  if (message) {
+    return next({ statusCode, message });
+  }
+  return res.status(statusCode).json(newProduct);
+}; */
