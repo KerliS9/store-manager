e0e6ee8fe17dc15acb3c7a923c923f97cecc9510
KerliS9 <kerlischroeder9@gmail.com>
2022-05-31 15:21:38 -0300

    fix - teste da camada serviço para adicionar produto

--- test/unit/controllers/productsControllersPost.js
+++ test/unit/controllers/productsControllersPost.js
@@ -4,13 +4,13 @@ const ProductsServices = require('../../../services/productsServices');
 const ProductsControllers = require('../../../controllers/productsControllers');
 const { productPayload } = require('../../../const/mockForTest');
 
-describe('Check controllers: where function is addNewProduct', () => {
+describe.only('Check controllers: where function is addNewProduct', () => {
   describe('when there is a product to add to the database', () => {
     const response = {};
     const request = {};
 
     before(() => {
-      request.body = { name: 'Martelo de Thor', quantity: 10 };
+      request.body = productPayload;
       response.status = sinon.stub().returns(response);
       response.json = sinon.stub().returns();
 
@@ -20,11 +20,13 @@ describe('Check controllers: where function is addNewProduct', () => {
       ProductsServices.addNewProduct.restore();
     });
     it('the method "status" is called with code 201', async () => {
-      await ProductsControllers.addNewProduct(request, response);
+      const resp = await ProductsControllers.addNewProduct(request, response);
+      console.log('controller status', resp);
       expect(response.status.calledWith(201)).to.be.true;
     });
     it('the method "json" should return an object', async () => {
-      await ProductsControllers.addNewProduct(request, response);
+      const resp = await ProductsControllers.addNewProduct(request, response);
+      console.log('controller json', resp);
       expect(response.json.calledWith(productPayload)).to.be.true;
     });
   });
@@ -43,10 +45,10 @@ describe('Check controllers: where function is addNewProduct', () => {
       ProductsServices.addNewProduct.restore();
     });
     it('the method "status" is called with code 404', async () => {
-      const test = await ProductsControllers.addNewProduct(request, response);
+      const test = await ProductsControllers.addNewProduct(request, response, next.next);
       // console.log('controller tests', test);
       expect(response.status.calledWith(404)).to.be.true;
-      expect(nex)
+      expect(nextSpy.calledWith({ statusCode: 409, message: 'Product already exists' }))
     });
     it('the method "json" should return an message', async () => {
       await ProductsControllers.addNewProduct(request, response);
--- test/unit/services/productsServicesPost.js
+++ test/unit/services/productsServicesPost.js
@@ -10,44 +10,53 @@ const { name } = productPayload;
 describe('Check Service Post: insert product on database', () => {
   describe('when the insertion happens successfully', () => {
     before(() => {
+      sinon.stub(ProductsModels, 'getProductByName').resolves(undefined);
       sinon.stub(ProductsModels, 'addNewProduct').resolves(productPayload);
     });
     after(() => {
+      ProductsModels.getProductByName.restore();
       ProductsModels.addNewProduct.restore();
     });
-    it('should return an object', async() => {
+    it('should return undefined', async() => {
       const response = await ProductsModels.getProductByName(name);
-      console.log('name on service', response);
-      expect(response).to.be.a('object');
+      // console.log('name on service', response);
+      expect(response).to.be.a('undefined');
     })
 
     it('should be an object', async () => {
       const response = await ProductsServices.addNewProduct(productPayload);
-      console.log('teste service', response);
+      // console.log('teste service', response);
       expect(response).to.be.an('object');
     });
     it('the object should have the keys id, name, quantity', async () => {
-      const response = await ProductsServices.addNewProduct(productPayload);
-      console.log('teste service keys', response);
-      expect(response).to.include.all.keys('id', 'name', 'quantity');
+      const { newProduct } = await ProductsServices.addNewProduct(productPayload);
+      // console.log('teste service keys', newProduct);
+      expect(newProduct).to.include.all.keys('id', 'name', 'quantity');
     });
     it('object should not to be empty', async () => {
-      const response = await ProductsServices.addNewProduct(productPayload);
-      console.log('teste service not empty', response);
-      expect(response).to.not.be.empty;
+      const { newProduct } = await ProductsServices.addNewProduct(productPayload);
+      // console.log('teste service not empty', newProduct);
+      expect(newProduct).to.not.be.empty;
     });
   });
 
-  /* describe('when product already exists on database', () => {
+  describe('when product already exists on database', () => {
     before(() => {
-      sinon.stub(ProductsModels, 'addNewProduct').resolves([[]]);
+      sinon.stub(ProductsModels, 'getProductByName').resolves(name);
+      sinon.stub(ProductsModels, 'addNewProduct').resolves(productPayload);
     });
     after(() => {
       ProductsModels.addNewProduct.restore();
     })
-    it('should return false', async () => {
-      const response = await ProductsServices.addNewProduct();
-      expect(response).to.be.false;
+    it('should return string', async() => {
+      const response = await ProductsModels.getProductByName(name);
+      // console.log('name on service', response);
+      expect(response).to.be.a('string');
     })
-  }) */
+    it('should return an object with the keys statusCode and message', async () => {
+      const response = await ProductsServices.addNewProduct(productPayload);
+      // console.log('produto já existe', response);
+      expect(response).to.include.all.keys('statusCode', 'message');;
+    })
+  })
 })
