bcf355bf76cb28d94276a738ccae2d3737f58dbf
KerliS9 <kerlischroeder9@gmail.com>
2022-07-04 20:28:14 -0300

    docs - documentation with swagger - routes products

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-// const express = require('express');
 const app = require('./app');
 require('dotenv').config();
-/* const { errorHandler } = require('./middlewares/errorHandler');
-const { productsRoutes, salesRoutes } = require('./routes');
-
-app.use(express.json());
-
-app.use('/products', productsRoutes);
-app.use('/sales', salesRoutes);
-
-app.use(errorHandler); */
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 app.listen(process.env.PORT, () => {
